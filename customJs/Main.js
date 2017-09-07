aa2b34576f76f18f6e44c5d38557571b273ab328
Aman Sharma <aman.11300733@lpu.in>
2017-09-07 21:07:44 +0530

    Changes made for cross browser

--- customJs/Main.js
+++ customJs/Main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   		$(".animateUp").each(function(){
     		var position = $(this).offset().top;
     		var topWindow = $(window).scrollTop();
-    		if (position < topWindow + 600) {
+    		if (position < topWindow + 500) {
       			$(this).addClass("slide");
     		}
   		});
