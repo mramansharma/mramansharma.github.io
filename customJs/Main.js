2bc817b2e0f6e86c9b4897bf70d6046da4d20b8b
Aman Sharma <aman.11300733@lpu.in>
2017-09-23 11:31:24 +0530

    Added new projects and skills

--- customJs/Main.js
+++ customJs/Main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(window).scroll(function() {
-  		$(".animateUp").each(function(){
+		$(".animateUp").each(function(){
     		var position = $(this).offset().top;
     		var topWindow = $(window).scrollTop();
     		if (position < topWindow + 600) {
